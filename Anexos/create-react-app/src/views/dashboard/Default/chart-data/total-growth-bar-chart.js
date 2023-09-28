f5a59e2107d601524c2fd687d5fd6e336bc94526
carolArrieta <107450192+carolArrieta@users.noreply.github.com>
2023-09-27 21:56:03 -0600

    Add files via upload

---
+++ Anexos/create-react-app/src/views/dashboard/Default/chart-data/total-growth-bar-chart.js
@@ -0,0 +1,87 @@
+// ===========================|| DASHBOARD - TOTAL GROWTH BAR CHART ||=========================== //
+
+const chartData = {
+  height: 480,
+  type: 'bar',
+  options: {
+    chart: {
+      id: 'bar-chart',
+      stacked: true,
+      toolbar: {
+        show: true
+      },
+      zoom: {
+        enabled: true
+      }
+    },
+    responsive: [
+      {
+        breakpoint: 480,
+        options: {
+          legend: {
+            position: 'bottom',
+            offsetX: -10,
+            offsetY: 0
+          }
+        }
+      }
+    ],
+    plotOptions: {
+      bar: {
+        horizontal: false,
+        columnWidth: '50%'
+      }
+    },
+    xaxis: {
+      type: 'category',
+      categories: ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes']
+    },
+    legend: {
+      show: true,
+      fontSize: '14px',
+      fontFamily: `'Roboto', sans-serif`,
+      position: 'bottom',
+      offsetX: 20,
+      labels: {
+        useSeriesColors: false
+      },
+      markers: {
+        width: 16,
+        height: 16,
+        radius: 5
+      },
+      itemMargin: {
+        horizontal: 15,
+        vertical: 8
+      }
+    },
+    fill: {
+      type: 'solid'
+    },
+    dataLabels: {
+      enabled: false
+    },
+    grid: {
+      show: true
+    }
+  },
+  series: [
+    {
+      name: 'Asistencias',
+      data: [20, 30, 25, 29, 18]
+    },
+    {
+      name: 'Participaciones',
+      data: [20, 35, 15, 33, 10]
+    },
+    {
+      name: 'Profit',
+      data: [35, 145, 35, 35, 20]
+    },
+    {
+      name: 'Maintenance',
+      data: [0, 0, 75, 0, 0]
+    }
+  ]
+};
+export default chartData;
---
+++ Anexos/create-react-app/src/views/dashboard/Default/index.js
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react';
+
+// material-ui
+import { Grid } from '@mui/material';
+
+// project imports
+import EarningCard from './EarningCard';
+import PopularCard from './PopularCard';
+import TotalOrderLineChartCard from './TotalOrderLineChartCard';
+//import TotalIncomeDarkCard from './TotalIncomeDarkCard';
+//import TotalIncomeLightCard from './TotalIncomeLightCard';
+import TotalGrowthBarChart from './TotalGrowthBarChart';
+import { gridSpacing } from 'store/constant';
+
+// ==============================|| DEFAULT DASHBOARD ||============================== //
+
+const Dashboard = () => {
+  const [isLoading, setLoading] = useState(true);
+  useEffect(() => {
+    setLoading(false);
+  }, []);
+
+  return (
+    <Grid container spacing={gridSpacing}>
+      <Grid item xs={12}>
+        <Grid container spacing={gridSpacing}>
+          <Grid item lg={6} md={6} sm={6} xs={12}>
+            <EarningCard isLoading={isLoading} />
+          </Grid>
+          <Grid item lg={6} md={6} sm={6} xs={12}>
+            <TotalOrderLineChartCard isLoading={isLoading} />
+          </Grid>
+          
+        </Grid>
+      </Grid>
+      <Grid item xs={12}>
+        <Grid container spacing={gridSpacing}>
+          <Grid item xs={12} md={7}>
+            <TotalGrowthBarChart isLoading={isLoading} />
+          </Grid>
+          <Grid item xs={12} md={5}>
+            <PopularCard isLoading={isLoading} />
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Dashboard;
