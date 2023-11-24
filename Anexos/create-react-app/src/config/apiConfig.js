f53a14cac15f709501ca0af0a27b2a4545b7029b
5100-chap <A01275100@tec.mx>
2023-11-24 13:49:15 -0600

    Deploy 14

--- Anexos/create-react-app/src/config/apiConfig.js
+++ Anexos/create-react-app/src/config/apiConfig.js
@@ -1,4 +1,4 @@
 const API_BASE_URL = 'http://147.185.221.17:18205';
-const OWN_BASE_URL = 'http://localhost:3000';
+const OWN_BASE_URL = 'https://brave-water-0d91eec10.4.azurestaticapps.net/';
 
 export { API_BASE_URL, OWN_BASE_URL };
