c71083a5b61bc6d7512eba2ca4d9d2e37406e835
5100-chap <A01275100@tec.mx>
2023-11-27 09:22:47 -0600

    ahh

--- Anexos/create-react-app/src/views/pages/authentication/authentication3/Login3.js
+++ Anexos/create-react-app/src/views/pages/authentication/authentication3/Login3.js
@@ -31,7 +31,7 @@ const Login = () => {
 
 		if (jwt) {
 			localStorage.setItem("jwt", jwt);
-			navigate("/inicio");
+			navigate("inicio");
 		} else {
 			const storedJwt = localStorage.getItem("jwt");
 			if (storedJwt && !isJwtExpired(storedJwt)) {
